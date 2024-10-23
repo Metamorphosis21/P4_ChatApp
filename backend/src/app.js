0117bc69005c0e4424e4dfe55a0dd2a394193d56
Anushka Pote <anushkapote1603@gmail.com>
2024-10-23 12:17:13 +0530

    Modified Code for 'User Profile Status Enhancement'

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,22 +1,17 @@
 import express from "express";
-import cors from"cors"
-import cookieParser from "cookie-parser"
-
-
-const app = express()
-
-
-app.use(express.json({limit: "20kb"}))
-app.use(express.urlencoded({extended: true, limit: "20kb"}))
-app.use(express.static("public"))
-
-app.use(cors({
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { updateUserProfile } from "../controllers/user.controller.js"; 
+const app = express();
+app.use(express.json({ limit: "20kb" }));
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+app.use(express.static("public"));
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-}))
-
-
-app.use(cookieParser())
-
-
-export {app};
+  })
+);
+app.use(cookieParser());
+app.put("/api/user/:id/update-profile", updateUserProfile); 
+export { app };
